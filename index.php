882c3b1ddc871ac13c6da6a9fe77967140add20e
nendifebrianto1 <nendifebrianto1@gmail.com>
2020-05-25 06:34:46 +0700

    Menmabhakan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 
 <body>
     <h1>Hello Word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse libero amet, vitae optio nostrum blanditiis quo,
+        unde officia explicabo tempore deserunt? Ut molestias provident saepe quos soluta! Quos, incidunt deserunt.</p>
 </body>
 
 </html>
